1fe69c7c383c565bddf43fc80b46fd5075179030
Salman-rahi <95650802+Salman-rahi@users.noreply.github.com>
2024-09-23 04:24:30 -0400

    history page update

--- script/card-1.js
+++ script/card-1.js
@@ -23,6 +23,18 @@ getDonation('donation-amount').addEventListener('click',function(){
             document.getElementById('amount').innerText = newAmount ;
             document.getElementById('card-btn-1').innerText =inputValue; 
             document.getElementById('donation-input-1').value = '';
+            const historyContainer =document.getElementById('history-container');
+            historyContainer.innerHTML=
+            `
+            
+            <section class=" sm:w-10/12 mx-auto my-5 px-5 py-5 text-left space-y-3 border-2 border-gray-300 rounded-2xl">
+               <h2 class="font-bold"><span>${inputValue}</span> Taka is Donated for famine-2024 at Feni, Bangladesh</h2>
+               <p>Date :${new Date().toLocaleString()}</p>
+            </section>
+            
+            
+            `
+
         }
         
 
